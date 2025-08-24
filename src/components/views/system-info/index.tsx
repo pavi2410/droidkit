772a5f9a634b9114826471c555ed7eafcf8113cb
Pavitra Golchha <hello@pavi2410.com>
2025-08-24 20:36:19 +0530

    fix scrolling

--- src/components/views/system-info/index.tsx
+++ src/components/views/system-info/index.tsx
@@ -29,7 +29,7 @@ export function SystemInfo({ selectedDevice }: SystemInfoProps) {
         </p>
       </div>
 
-      <ScrollArea className="flex-[1_1_0]">
+      <ScrollArea className="flex-1 h-0">
         <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 pb-4">
           <DeviceInfoCard selectedDevice={selectedDevice} />
           <HardwareInfoCard selectedDevice={selectedDevice} />
