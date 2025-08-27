838e69af7055d7a5fb45e619d4867f9b95d919ef
Pavitra Golchha <hello@pavi2410.com>
2025-08-27 22:18:45 +0530

    install tauri-plugin-decorum

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use tauri::Manager;
+use tauri_plugin_decorum::WebviewWindowExt;
+
 use crate::adb_commands::{
     DeviceInfo, DiscoveredDevice, DiscoveredWirelessDevice, FileInfo, PairingData,
     connect_tcp_device, connect_to_discovered_device, discover_wireless_devices,
@@ -258,8 +261,33 @@ async fn get_device_network_info_cmd(device_serial: String) -> Result<NetworkInf
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_decorum::init()) // initialize the decorum plugin
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+			// Create a custom titlebar for main window
+			// On Windows this hides decoration and creates custom window controls
+			// On macOS it needs hiddenTitle: true and titleBarStyle: overlay
+			let main_window = app.get_webview_window("main").unwrap();
+			main_window.create_overlay_titlebar().unwrap();
+
+			// Some macOS-specific helpers
+			#[cfg(target_os = "macos")] {
+				// Set a custom inset to the traffic lights
+				main_window.set_traffic_lights_inset(12.0, 16.0).unwrap();
+
+				// Make window transparent without privateApi
+				main_window.make_transparent().unwrap();
+
+				// // Set window level
+				// // NSWindowLevel: https://developer.apple.com/documentation/appkit/nswindowlevel
+				// main_window.set_window_level(25).unwrap();
+
+                ()
+			}
+
+			Ok(())
+		})
         .invoke_handler(tauri::generate_handler![
             device_info,
             get_android_sdk_path,
